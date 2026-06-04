571c8629143b6834ae429b46c9f259fe5dce0202
fpihcmtools <fpihcm.tools@gmail.com>
2026-06-04 21:25:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 // Lưu trữ các thông tin cấu hình nhạy cảm hoặc cấu hình môi trường
-const supabaseUrl = 'https://moahaaubepvvpkukjoun.supabase.co';
+const supabaseUrl = 'https://vghvhyljpomlkydhihvw.supabase.co';
 // WARNING: Điền Anon Key vào thay cho process.env.SUPABASE_KEY vì file HTML chạy trên trình duyệt client
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWhhYXViZXB2dnBrdWtqb3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMjk2NTksImV4cCI6MjA5MTkwNTY1OX0.AgY0-QhMeTXF08eCBRnIxyAjq8mtNHYQBDr6F5jqPcQ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnaHZoeWxqcG9tbGt5ZGhpaHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NzEyNzcsImV4cCI6MjA5NjE0NzI3N30.3yhdpcrTZoINHludQ0O9iQgRpoFKrCZIXKMHILHFvS4';
